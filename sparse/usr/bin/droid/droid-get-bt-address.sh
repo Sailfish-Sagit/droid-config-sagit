e12a60fe2c56731d2db36b41b86ce9b28897355b
twoxa <98216123+ttwoxa@users.noreply.github.com>
2024-02-13 23:15:20 +0300

    Fix bluetooth.

---
+++ sparse/usr/bin/droid/droid-get-bt-address.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# If the bluetooth address is provided by another script use that
+if [ -f /var/lib/bluetooth/board-address ] ; then
+            exit 0
+fi
+
+mkdir -p /var/lib/bluetooth
+
+# Getting address file from properties
+binary_addr=$(xxd -p /mnt/vendor/persist/wlan_bt/bt.mac); bt_addr=$(echo $binary_addr | sed 's/../&:/g; s/:$//'); echo $bt_addr
+if [ $bt_addr != "" ]; then
+            echo $bt_addr > /var/lib/bluetooth/board-address
+                if [ ! -f /var/lib/bluetooth/board-address ]; then
+                                echo "Failed to set bluetooth address."
+                                        exit 1
+                                            fi
+                                    else
+                                                echo "Failed to get bluetooth address!"
+                                                    exit 1
+fi
+
+# Set proper permissions
+chown root:root /var/lib/bluetooth/board-address
+chmod 644 /var/lib/bluetooth/board-address
+exit 0
